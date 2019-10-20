aee4124add1f8760c5e71c7ce7b9ee4ed2721199
Kővágó Sándor <56760241+kvgsndr@users.noreply.github.com>
2019-10-20 13:30:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iskola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ember e1 = new Ember();
+            Ember e2 = new Ember("Jani", 25, "Férfi");
+            Tanulo t1;
+            t1 = new Tanulo();
+
+            e1.Bemutatkozik();
+            e2.Bemutatkozik();
+            t1.Bemutatkozik();
+            Console.ReadKey();
+               
+        }
+    }
+}
---
+++ tanulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iskola
+{
+    class Tanulo :Ember
+    {
+        string elozoIskola;
+        int hianyzottNapok;
+        public override void getCel()
+        {
+            Console.WriteLine("Túljutni az érettségin! ");
+        }
+
+        public override void Bemutatkozik()
+        {
+            Console.WriteLine("Szia, {0} vagyok, {1} éves {2}, a(z) {3} intézményéből jöttem, {4} napot hiányoztam eddig.\n", nev, kor, nem, elozoIskola, hianyzottNapok);
+
+        }
+
+        public Tanulo() : base()
+        {
+            elozoIskola = "Türr István";
+            hianyzottNapok = 10;
+        }
+
+    }
+}
